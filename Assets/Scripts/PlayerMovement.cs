b0c1a54fb1ffe35c4fd544fbece07f2f632a39dd
Алексей Григарович <61623090+Alexei-Grigarovich@users.noreply.github.com>
2021-06-12 00:05:17 +0300

    Add player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Transform playerTransform;
+
     public float speed;
+
+    private Vector2 direction;
+    private void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+    }
 
     void FixedUpdate()
     {
-        
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+        playerTransform.Translate(direction * speed * Time.deltaTime);
     }
 }
