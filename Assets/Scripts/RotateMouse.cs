7d8e9c5d19c0cd27fa62ce627048c37ec7e2eb92
Алексей Григарович <61623090+Alexei-Grigarovich@users.noreply.github.com>
2021-06-12 04:11:23 +0300

    Add rotate by mouse

---
+++ Assets/Scripts/RotateMouse.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMouse : MonoBehaviour
+{
+    private Transform playerTransform;
+    private Camera cameraMain;
+
+    public float rotateSpeed;
+
+    private Vector3 mousePosition;
+
+    private Vector2 targetDestination;
+    private float targetRotation;
+    //private Vector2 targetPosition;
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+
+        cameraMain = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        mousePosition = cameraMain.ScreenToWorldPoint(Input.mousePosition);
+
+        targetDestination = mousePosition - playerTransform.position;
+        targetRotation = Mathf.Atan2(targetDestination.y, targetDestination.x) * Mathf.Rad2Deg;
+        playerTransform.rotation = Quaternion.Lerp(playerTransform.rotation, 
+            Quaternion.AngleAxis(targetRotation, Vector3.forward), rotateSpeed * Time.deltaTime);
+    }
+}
